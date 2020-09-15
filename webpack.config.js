eb20fec3b5a789fc544dbb670603bbbcb281d900
Callisto <callisto2410@yandex.ru>
2020-09-15 18:49:26 +0900

    Updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'test', 'dist'),
     },
+    devtool: 'source-map',
     module: {
         rules: [
             {
