df382913ad67fd12437a73e1d1d0384a25297af2
Callisto <callisto2410@yandex.ru>
2020-09-21 11:43:10 +0900

    Added "mode" option

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
         path: path.resolve(__dirname, 'test', 'dist'),
     },
     devtool: 'source-map',
+    mode: 'production',
     module: {
         rules: [
             {
