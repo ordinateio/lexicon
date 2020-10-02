2babd25412876ce6559c54c92e6c947e33c60739
Callisto <callisto2410@yandex.ru>
2020-10-02 17:38:40 +0900

    Updated build system

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -15,7 +15,7 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: "awesome-typescript-loader"
+                use: "ts-loader",
             },
         ],
     },
