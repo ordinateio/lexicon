3dd85be153ecd01ee80417514bcfa3f220d7e104
Callisto <callisto2410@yandex.ru>
2020-11-30 16:53:21 +0900

    Added test for missing key

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -20,6 +20,9 @@ if (!placeholders_ru) throw new Error('Element ".placeholders_ru" is "undefined"
 
 let placeholders_text: string;
 
+const missing_key = document.querySelector('.content .missing-key');
+if (!missing_key) throw new Error('Element ".missing-key" is "undefined".');
+
 Lexicon.extend({
     lorem: {
         en: 'However, we should not forget that the constant quantitative growth and scope of our activity contributes to the preparation and implementation of the positions taken by the participants in relation to the tasks set. It should not be forgotten, however, that the scope and place of personnel training entails the process of introducing and modernizing new proposals.',
@@ -94,3 +97,9 @@ placeholders_text = Lexicon.format(
 placeholders_ru.innerHTML = `
 <h1>Placeholders: ru</h1>
 <p>${placeholders_text}</p>`;
+
+
+/* Non existing key. */
+missing_key.innerHTML = `
+<h1>Missing key</h1>
+<p>${Lexicon.get('missing_key')}</p>`;
