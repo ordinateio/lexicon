5b242bf1fddafe377df6a001220f0b19328d6cf3
Ordinate IO <ordinateio@gmail.com>
2021-07-20 13:19:48 +0900

    Improve test

--- test/index.test.ts
+++ test/index.test.ts
@@ -10,7 +10,9 @@ describe('Translations ...', () => {
     });
 
     test('The language is set to "de".', () => {
-        expect(lexicon.locale).toBe('de');
+        const value = lexicon.locale;
+
+        expect(value).toBe('de');
     });
 
     lexicon.extend({
@@ -20,7 +22,9 @@ describe('Translations ...', () => {
     });
 
     test('Adding the language "en".', () => {
-        expect('en' in lexicon.translations['test']).toBe(true);
+        const value = 'en' in lexicon.translations['test'];
+
+        expect(value).toBe(true);
     });
 
     lexicon.extend({
@@ -30,7 +34,9 @@ describe('Translations ...', () => {
     });
 
     test('Adding the language "ru".', () => {
-        expect('ru' in lexicon.translations['test']).toBe(true);
+        const value = 'ru' in lexicon.translations['test'];
+
+        expect(value).toBe(true);
     });
 
     lexicon.extend({
@@ -40,7 +46,9 @@ describe('Translations ...', () => {
     });
 
     test('Adding the language "de".', () => {
-        expect('de' in lexicon.translations['test']).toBe(true);
+        const value = 'de' in lexicon.translations['test'];
+
+        expect(value).toBe(true);
     });
 });
 
@@ -56,20 +64,23 @@ describe('Matching translations ...', () => {
 
     test('Language "en".', () => {
         lexicon.locale = 'en';
+        const value = lexicon.get('test');
 
-        expect(lexicon.get('test')).toContain(en);
+        expect(value).toContain(en);
     });
 
     test('Language "ru".', () => {
         lexicon.locale = 'ru';
+        const value = lexicon.get('test');
 
-        expect(lexicon.get('test')).toContain(ru);
+        expect(value).toContain(ru);
     });
 
     test('Language "de".', () => {
         lexicon.locale = 'de';
+        const value = lexicon.get('test');
 
-        expect(lexicon.get('test')).toContain(de);
+        expect(value).toContain(de);
     });
 });
 
